90bdbb53cd1091e9f18f0379a6c1faef54c0fe2d
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-16 12:27:57 +0530

    Added one more list helper function and corresponding tests
    Completed Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,7 +2,19 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((total, blog) => total + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const favBlog = blogs.reduce((fav, blog) => ((blog.likes > fav.likes) ? blog : fav), blogs[0]);
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
