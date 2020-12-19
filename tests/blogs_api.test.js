fb3d289b8ae223210a749f9763614881de6639a4
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-19 17:56:26 +0530

    refactored blogs_api.test.js

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -16,86 +16,89 @@ beforeEach(async () => {
   await Promise.all(promises);
 });
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/);
-});
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
 
-test('all notes are returned', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200);
-  expect(response.body)
-    .toHaveLength(helper.initialBlogs.length);
-});
+  test('all blogs are returned', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200);
+    expect(response.body)
+      .toHaveLength(helper.initialBlogs.length);
+  });
 
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-  };
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd)
-    .toHaveLength(helper.initialBlogs.length + 1);
-});
-
-test('unique identifier property of the blog posts is named id', async () => {
-  const response = await api
-    .get('/api/blogs');
-  response.body.forEach((blog) => {
-    expect(blog.id)
-      .toBeDefined();
+  test('blogs have identifier property named id', async () => {
+    const response = await api
+      .get('/api/blogs');
+    response.body.forEach((blog) => {
+      expect(blog.id)
+        .toBeDefined();
+    });
   });
 });
 
-test('likes property is defaulted to zero', async () => {
-  const newBlog = {
-    title: 'This blog is not Liked',
-    author: 'Jayanth PSY',
-    url: 'https://localhost:8080',
-  };
+describe('addition of a new blog', () => {
+  test('succeeds with valid data', async () => {
+    const newBlog = {
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+    };
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201);
-  expect(response.body.likes)
-    .toBe(0);
-});
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
 
-test('title is mandatory for a blog', async () => {
-  const invalidBlog = {
-    author: 'Jayanth PSY',
-    url: 'https://localhost:8080',
-    likes: 5,
-  };
-  await api
-    .post('/api/blogs')
-    .send(invalidBlog)
-    .expect(400);
-});
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd)
+      .toHaveLength(helper.initialBlogs.length + 1);
+  });
+  test('fails without a title', async () => {
+    const invalidBlog = {
+      author: 'Jayanth PSY',
+      url: 'https://localhost:8080',
+      likes: 5,
+    };
+    await api
+      .post('/api/blogs')
+      .send(invalidBlog)
+      .expect(400);
+  });
 
-test('url is mandatory for a blog', async () => {
-  const invalidBlog = {
-    title: 'Is url field mandatory?',
-    author: 'Jayanth PSY',
-    likes: 1,
-  };
-  await api
-    .post('/api/blogs')
-    .send(invalidBlog)
-    .expect(400);
+  test('fails without a url', async () => {
+    const invalidBlog = {
+      title: 'Is url field mandatory?',
+      author: 'Jayanth PSY',
+      likes: 1,
+    };
+    await api
+      .post('/api/blogs')
+      .send(invalidBlog)
+      .expect(400);
+  });
+
+  test('with no likes field gets likes: 0 by default', async () => {
+    const newBlog = {
+      title: 'This blog is not Liked',
+      author: 'Jayanth PSY',
+      url: 'https://localhost:8080',
+    };
+
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201);
+    expect(response.body.likes)
+      .toBe(0);
+  });
 });
 
 afterAll(async () => {
