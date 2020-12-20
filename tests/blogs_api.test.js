d57fce8f6313829ba8999a6aaa981f999f76a5d3
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2020-12-20 16:57:57 +0530

    refactored blogs_api.test.js

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -6,17 +6,17 @@ const Blog = require('../models/blog');
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await Blog.deleteMany({});
-
-  const blogObjs = helper.initialBlogs
-    .map((blog) => new Blog(blog));
-
-  const promises = blogObjs.map((blogObj) => blogObj.save());
-  await Promise.all(promises);
-});
-
 describe('when there is initially some blogs saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    const blogObjs = helper.initialBlogs
+      .map((blog) => new Blog(blog));
+
+    const promises = blogObjs.map((blogObj) => blogObj.save());
+    await Promise.all(promises);
+  });
+
   test('blogs are returned as json', async () => {
     await api
       .get('/api/blogs')
@@ -40,120 +40,121 @@ describe('when there is initially some blogs saved', () => {
         .toBeDefined();
     });
   });
-});
 
-describe('addition of a new blog', () => {
-  test('succeeds with valid data', async () => {
-    const newBlog = {
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-    };
+  describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+      const newBlog = {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+      };
 
-    await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/);
+      await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
 
-    const blogsAtEnd = await helper.blogsInDb();
-    expect(blogsAtEnd)
-      .toHaveLength(helper.initialBlogs.length + 1);
-  });
-  test('fails without a title', async () => {
-    const invalidBlog = {
-      author: 'Jayanth PSY',
-      url: 'https://localhost:8080',
-      likes: 5,
-    };
-    await api
-      .post('/api/blogs')
-      .send(invalidBlog)
-      .expect(400);
-  });
+      const blogsAtEnd = await helper.blogsInDb();
+      expect(blogsAtEnd)
+        .toHaveLength(helper.initialBlogs.length + 1);
+    });
 
-  test('fails without a url', async () => {
-    const invalidBlog = {
-      title: 'Is url field mandatory?',
-      author: 'Jayanth PSY',
-      likes: 1,
-    };
-    await api
-      .post('/api/blogs')
-      .send(invalidBlog)
-      .expect(400);
-  });
+    test('fails without a title', async () => {
+      const invalidBlog = {
+        author: 'Jayanth PSY',
+        url: 'https://localhost:8080/no-title',
+        likes: 5,
+      };
+      await api
+        .post('/api/blogs')
+        .send(invalidBlog)
+        .expect(400);
+    });
 
-  test('with no likes field gets likes: 0 by default', async () => {
-    const newBlog = {
-      title: 'This blog is not Liked',
-      author: 'Jayanth PSY',
-      url: 'https://localhost:8080',
-    };
+    test('fails without a url', async () => {
+      const invalidBlog = {
+        title: 'Is url field mandatory?',
+        author: 'Jayanth PSY',
+        likes: 1,
+      };
+      await api
+        .post('/api/blogs')
+        .send(invalidBlog)
+        .expect(400);
+    });
 
-    const response = await api
-      .post('/api/blogs')
-      .send(newBlog)
-      .expect(201);
-    expect(response.body.likes)
-      .toBe(0);
-  });
-});
+    test('with no likes field gets likes: 0 by default', async () => {
+      const newBlog = {
+        title: 'This blog is not Liked',
+        author: 'Jayanth PSY',
+        url: 'https://localhost:8080/no-likes',
+      };
 
-describe('deletion of a blog', () => {
-  test('succeeds with an existing id', async () => {
-    const blogs = await helper.blogsInDb();
-    const blogToDelete = blogs[Math.round(Math.random() * blogs.length)];
-
-    await api
-      .delete(`/api/blogs/${blogToDelete.id}`)
-      .expect(204);
-
-    const blogsNew = await helper.blogsInDb();
-    expect(blogsNew.length)
-      .toBe(blogs.length - 1);
-  });
-});
-
-describe('updating likes of a blog', () => {
-  test('succeeds with an existing id and responds with the same updated information', async () => {
-    const blogs = await helper.blogsInDb();
-    const blogToUpdate = blogs[Math.round(Math.random() * blogs.length)];
-    const updatedBlog = {
-      ...blogToUpdate,
-      likes: blogToUpdate.likes + Math.round(Math.random() * 10),
-    };
-    delete updatedBlog.id;
-
-    await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(updatedBlog)
-      .expect(200, {
-        ...updatedBlog,
-        id: blogToUpdate.id,
-      });
+      const response = await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201);
+      expect(response.body.likes)
+        .toBe(0);
+    });
   });
 
-  test('changes reflect in the database with an existing id', async () => {
-    const blogs = await helper.blogsInDb();
-    const blogToUpdate = blogs[Math.round(Math.random() * blogs.length)];
-    const updatedBlog = {
-      ...blogToUpdate,
-      likes: blogToUpdate.likes + Math.round(Math.random() * 10),
-    };
-    delete updatedBlog.id;
+  describe('deletion of a blog', () => {
+    test('succeeds with an existing id', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToDelete = blogs[Math.round(Math.random() * blogs.length)];
 
-    await api
-      .put(`/api/blogs/${blogToUpdate.id}`)
-      .send(updatedBlog);
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204);
 
-    const blogsNew = await helper.blogsInDb();
-    expect(blogsNew)
-      .toContainEqual({
-        ...updatedBlog,
-        id: blogToUpdate.id,
-      });
+      const blogsNew = await helper.blogsInDb();
+      expect(blogsNew.length)
+        .toBe(blogs.length - 1);
+    });
+  });
+
+  describe('updating likes of a blog', () => {
+    test('succeeds with an existing id and responds with the same updated information', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToUpdate = blogs[Math.round(Math.random() * blogs.length)];
+      const updatedBlog = {
+        ...blogToUpdate,
+        likes: blogToUpdate.likes + Math.round(Math.random() * 10),
+      };
+      delete updatedBlog.id;
+
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog)
+        .expect(200, {
+          ...updatedBlog,
+          id: blogToUpdate.id,
+        });
+    });
+
+    test('changes reflect in the database with an existing id', async () => {
+      const blogs = await helper.blogsInDb();
+      const blogToUpdate = blogs[Math.round(Math.random() * blogs.length)];
+      const updatedBlog = {
+        ...blogToUpdate,
+        likes: blogToUpdate.likes + Math.round(Math.random() * 10),
+      };
+      delete updatedBlog.id;
+
+      await api
+        .put(`/api/blogs/${blogToUpdate.id}`)
+        .send(updatedBlog);
+
+      const blogsNew = await helper.blogsInDb();
+      expect(blogsNew)
+        .toContainEqual({
+          ...updatedBlog,
+          id: blogToUpdate.id,
+        });
+    });
   });
 });
 
