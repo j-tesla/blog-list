dd9419ec78031377b39b92089efa857e660efcb7
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2021-06-30 17:08:19 +0530

    using static build folder

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ mongoose.connect(config.mongoUrl, {
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(express.static('build'));
 app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
