7420b9e729a127f81e216d1df49d5717225444f4
Jayanth PSY <58279279+j-tesla@users.noreply.github.com>
2021-06-30 17:23:53 +0530

    added build

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
+const path = require('path');
 
 mongoose.connect(config.mongoUrl, {
   useNewUrlParser: true,
@@ -20,20 +21,25 @@ mongoose.connect(config.mongoUrl, {
   useCreateIndex: true,
 }).then(() => {
   logger.info(`connected to ${config.mongoUrl}`);
-}).catch(((reason) => {
-  logger.error(`failed to connect to${config.mongoUrl}`);
-  logger.error(reason);
-  process.exit(500);
-}));
+})
+  .catch(((reason) => {
+    logger.error(`failed to connect to${config.mongoUrl}`);
+    logger.error(reason);
+    process.exit(500);
+  }));
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
-app.use(express.static('build'));
 app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
+
+app.use(express.static(`${__dirname}/build`));
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 if (process.env.NODE_ENV === 'test') {
   // eslint-disable-next-line global-require
